8f3e8beb2dbbd748d190041bb26a8f32172af1d2
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-12-10 02:56:58 +0900

    line_foods_controllerの作成

---
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -0,0 +1,48 @@
+module Api
+  module V1 
+    class LineFoodsController < ApplicationController
+      before_action :set_food, only: %i[create]
+
+      def create
+        if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
+          return render json: {
+            existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
+            new_restaurant: Food.find(params[:food_id]).restaurant.name,
+          }, status: :not_acceptable
+        end
+
+        set_line_food(@ordered_food)
+
+        if @line_food.save
+          render json: {
+            line_food: @line_food
+          }, status: :created
+        else
+          render json: {}, status: :initernal_server_error
+        end
+
+        private
+
+        def set_food
+          @ordered_food = Food.find(params[:food_id])
+        end
+
+        def set_line_food(orderd_food)
+          if orderd_food.line_food.present?
+            @line_food = orderd_food.line_food
+            @line_food.attributes = {
+              count: oederd_food.line_food.count + params[:count],
+              active: true
+            }
+          else
+            @line_food = orderd_food.build_line_food(
+              count: params[:count],
+              restaurant: orderd_food.restaurant,
+              active: true
+            )
+          end
+        end
+      end
+    end
+  end
+end
